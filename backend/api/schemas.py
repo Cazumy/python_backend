f57aa0c0407074591d749e44b053e89d3899941b
cazumy <mr-4erkasov@yandex.ru>
2025-07-08 15:07:00 +0300

    init

--- backend/api/schemas.py
+++ backend/api/schemas.py
@@ -1,11 +1,12 @@
 from datetime import date
 from pydantic import BaseModel
+from typing import Optional
 
 class RollCreate(BaseModel):
     length: float
     weight: float
-    date_of_adding: date = None
-    date_of_removing: date = None
+    date_of_adding: Optional[date] = None
+    date_of_removing: Optional[date] = None
 
 class RollRead(RollCreate):
     id: int
